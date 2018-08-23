27883671e8fb6c9ec9a1733da250be3293c4d6cd
Jishnu Chakrabarti <jishnu.chakrabarti@gmail.com>
2018-08-23 21:42:32 +0400

    Update cpld.cpp

--- cpld.cpp
+++ cpld.cpp
@@ -1,38 +1,47 @@
 //g++ cpld.cpp -lwiringPi -o cpld
+// core used is gz_24o1I.xsvf
 
 //-------Headers----------------------
 #include <iostream>
 #include <wiringPiSPI.h>
 #include <unistd.h>
 
-//--------Global variables---------------
+//---------Introduction about SPI------
 // The Serial Peripheral Interface (SPI) is a communication protocol used to 
 // transfer data between micro-computers like the Raspberry Pi and peripheral devices.
 // These peripheral devices may be either sensors or actuators.
+
 // SPI uses 4 separate connections to communicate with the peripheral device. 
-// These connections are (a) serial clock (CLK), (b) Master Input Slave Output (MISO), 
-// (c) Master Output Slave Input (MOSI) and Chip Select (CS).
+// These connections are 
+// (a) serial clock (CLK), 
+// (b) Master Input Slave Output (MISO), 
+// (c) Master Output Slave Input (MOSI) and 
+// (d) Chip Select (CS).
+
 // The clock pin sense pulses at a regular frequency, the speed at which the 
 // Raspberry Pi and SPI device agree to transfer data to each other.
+
 // The MISO pin is a data pin used for the master (in this case the Raspberry Pi) 
 // to receive data from the peripheral data. Data is read from the bus after every clock pulse.
+
 // The MOSI pin sends data from the Raspberry Pi to the peripheral device. 
 // The device will take the value of the bus on the rising / falling  edge of the clock. 
 // This means the value must be set before the clock is pulsed.
+
 // the Chip Select line chooses which particular SPI device is in use. 
-// If there are multiple SPI devices, they can all share the same CLK, MOSI, and MISO. 
-// Only the selected device has the Chip Select line set low, 
-// while all other devices have their CS lines set high. 
+// If there are multiple SPI devices, they can all share the same CLK, MOSI, and MISO.
+
+// Only the selected device has the Chip Select line set low, while all other devices have their CS lines set high. 
 // A high Chip Select line tells the SPI device to ignore all of the commands and traffic on the rest of the bus.
-// Here the CHANNEL is CE / CS in Raspberry Pi SPI. it can be 0 or 1, depending upon which
-// pin its connected. CPLD is connected to CE0.
+
 // The SPI bus is available on the P1 Header:
 
 // MOSI    P1-19
 // MISO    P1-21
-// SCLK    P1-23   P1-24    CE0
-// GND     P1-25   P1-26    CE1
+// SCLK    P1-23   P1-24    CE0 ---> CPLD
+// GND     P1-25   P1-26    CE1 ---> NA
 
+//--------Global variables---------------
 const int CHANNEL = 0;
 unsigned char buffer[2];
 int result;
@@ -62,6 +71,7 @@ int main()
 	// Configure the interface.
    	// CHANNEL insicates chip select,
    	// 500000 indicates bus speed.
+	
    	fd = wiringPiSPISetup(CHANNEL, 500000);
 
 	printVal("Init Result");
